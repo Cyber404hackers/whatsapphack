79bc669594a3751308bee376bdc5cff8c1bdd38d
Cyber404hackers <90260593+Cyber404hackers@users.noreply.github.com>
2021-09-07 15:13:35 +0200

    Add files via upload

---
+++ whatsapphack.py
@@ -0,0 +1,54 @@
+import time
+localtime = time.asctime( time.localtime(time.time()) )
+import random
+w='''
+______________________whatsHack__________________
+________________Cyber404★★cyber404______________
+
+version:  Prank
+              ✧══════■■■■■■■■■■■■■
+               ╔═══ılı.lıllılı.ıllı.═══╗
+               ■■■■■■■■■■■■■■■■■■■■
+               ❁Whatsapp hacking tool❁
+                ═══ılı.lıllılı.ıllı.═══
+                □□□□□□□□□□□□□□□□□□□
+               ●✧☆Cyber404☆ᴄᴏᴘʏʀɪɢʜᴛ✧
+               ■■ ║▌│█║▌│ █║▌│█│║▌║
+      
+___________________Done by Cyber404______________
+
+Disclaimer:this 'is' a fake whatsapp hacking tool,so don't use it to scamm others
+'''  
+print(w)
+print('''
+Other Tools Available..............
+________________________________________________ 
+
+  [01]Facebook hacking            [02]Instagram
+  [03]Twitter hacking             [04]Yutube
+  [05]Telegram hacking            [06]Google
+  [07]Netflixx                    [08]Sasai
+  [09]Signal                      [10]Yahoo
+  [11]Yandex                      [13]Pintrest
+  [0]Exit         [99]Contact Cyber404
+  
+         》》》》》》》》》》》》》》》》》
+  _______Whatsapp hacking tool Active____________
+  ''')
+  
+a=input('Enter whatsapp number of your victim\n\n\n')
+b=0
+c=a[0]
+while (b<100000):
+    print('Getting the verification code of victim!Progress',b)
+    b = b + 1
+else:
+    print('''\n\nThe verification code is the number below:\n\n\n
+   ________________■__________________■_______________________Cyber404 hacks___________■_____________
+    
+    ''',random.randint(100000,200000))
+    print("""_____________________》_________《________
+    """)
+    print('Scan was sucessful,done on:',localtime)
+    print('\n\n\nHaaa haaa bvuma kupusa iwe put that verification code\n\n')
+    
